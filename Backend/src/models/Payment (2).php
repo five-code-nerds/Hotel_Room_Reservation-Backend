91c1e5443859c2e13eed463a728ff5f325eb586c
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-05-04 15:10:04 +0100

    payment is added

---
+++ Backend/src/models/Payment (2).php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Src\Models;
+
+use Src\Core\Database;
+use PDO;
+
+class Payment
+{
+    private PDO $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    public function createPayment($data)
+    {
+        $stmt = $this->db->prepare("
+            INSERT INTO payments
+            (reservation_id, amount, payment_method, transaction_ref , payment_status)
+            VALUES (?, ?, ?, ?, ?)
+        ");
+
+        $stmt->execute([
+            $data["reservation_id"],
+            $data["amount"],
+            $data["payment_method"],
+            $data["transaction_ref"],
+            $data["payment_status"]
+        ]);
+
+        return $this->db->lastInsertId();
+    }
+
+    public function findPaymentByTransactionRef($transaction_ref)
+    {
+        $stmt = $this->db->prepare("
+            SELECT * FROM payments WHERE transaction_ref = ?
+        ");
+
+        $stmt->execute([$transaction_ref]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updatePaymentStatus($transaction_ref, $status)
+    {
+        $stmt = $this->db->prepare("
+            UPDATE payments SET payment_status = ? WHERE transaction_ref = ?
+        ");
+
+        $stmt->execute([$status, $transaction_ref]);
+    }
+}
+
+?>
